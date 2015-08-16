155072f93f9ac6c99b92359090ecdc4226eec01d
arciel <sjd96bvn@gmail.com>
2015-08-16 13:59:37 +0530

    Prettified code somewhat.

--- Program.cpp
+++ Program.cpp
@@ -1,39 +1,48 @@
 // Program.cpp : Defines the entry point for the console application.
 
-#define _CRT_ARGUMENT_NO_WARNINGS
 
-//#include "stdafx.h"
 #include <stdio.h>
 #include <tchar.h>
 #include <iostream>
+#include <SDL.h>
+
 #include "VParticle.h"
 #include <vector>
 
-/* Bug: 
+
+#pragma comment(lib,"SDL2main.lib")
+#pragma comment(lib,"SDL2.lib")
+
+
+/* Bugs: 
  *		1. Breaks if + and - charges entered at the exact same place.
  *
- * To-do: 
+ * To-do List: 
  *		1. Fix bugs (if any).
- *		2. Implement cutoff distance.
+		   Draw cartesian grid.
+		   Implement particle "tracking", i.e. show particle stats on screen.
+		   
+ *		2. Custom Properties.
+ *      3. Custom, user defined force function.
  */
 
-double time, delta;         // Current time and dt (step size).
-float nx_q;				    // Charge and ...
-int nx_s;				    // ... sign of next particle to be added.
-int fixed;			        // Whether or not the charge is fixed in place.
-float cutOffDistance;		// Any object beyond this distance from the screen center will be deleted.
+int SC_WIDTH = 800;
+int SC_HEIGHT = 600;
 
-Vector2D screenCentre(400, 300);
+double delta; // Current time and dt (step size).
+float nx_q;	  // Charge and ...
+int nx_s;	  // ... sign of next particle to be added.
+int fixed;	  // Whether or not the charge is fixed in place.
+
 
 std::vector<VParticle *> List;        
 
-void verlet_integrate();
 void rk4_integrate();
+void load_config_file();
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::cout << "Enter cutoff distance: ";
-	std::cin >> cutOffDistance;
+
 	nx_q = 1.0;
 	nx_s = 1;
 	puts("Init SDL...\n");
@@ -42,19 +51,15 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_SetHint(SDL_HINT_RENDER_DRIVER, "opengl");
-	w = SDL_CreateWindow("q-sim.", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+	w = SDL_CreateWindow("q-sim.", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SC_WIDTH, SC_HEIGHT, 0);
 	r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED);
 
 	bool quit = false;
-	time = 0;					// Set the universe's time to zero and begin simulation.
-	delta = 1.0 / 60.0;			// Each frame advances the time by delta amount.
 	
-	printf("NumDrv=%d\n", SDL_GetNumRenderDrivers());
-	SDL_RendererInfo inf;                            
-	SDL_GetRendererInfo(r, &inf);                    
-	                                                 
-	printf("Current Renderer = %s\n", inf.name);     
-	printf("Current Video Driver = %s\n", SDL_GetCurrentVideoDriver());
+	delta = 1.0 / 60.0;			// Each frame advances the time by delta amount.
+
+	//load_config_file();
+
 
 	while (!quit)	// GUI Handling.
 	{
@@ -68,17 +73,21 @@ int _tmain(int argc, _TCHAR* argv[])
 				if (nx_q < 1) nx_q = 1;
 				printf("Next charge = %f\n", nx_q);
 			}
+			/*
+			SDL's origin is at the top-left of the onscreen window. X increments to the right and Y increments downwards.
+			We want our origin to be at the centre of the screen, so we shift the X and Y coords of particles as they get added.
+			*/
 			if (e.type == SDL_MOUSEBUTTONUP)
 			{
 				if (e.button.button == SDL_BUTTON_LEFT)			// Left click, add a new +ve particle to the sim.
 				{
-					printf("Add charge +%fq at %d %d\n", nx_q, e.button.x, e.button.y);
-					List.push_back(new VParticle(List.size(), e.button.x, e.button.y, e.button.x, e.button.y, -nx_q, fixed));
+					printf("Add charge +%fq at %d %d\n", nx_q, e.button.x - (SC_WIDTH/2), e.button.y - (SC_HEIGHT/2));
+					List.push_back(new VParticle(List.size(), e.button.x - (SC_WIDTH / 2), e.button.y - (SC_HEIGHT / 2), e.button.x - (SC_WIDTH / 2), e.button.y - (SC_HEIGHT / 2), nx_q, fixed));
 				}
 				else if (e.button.button == SDL_BUTTON_RIGHT)   // Right click, add -ve particle to the sim.
 				{
-					printf("Add charge -%fq at %d %d\n", nx_q, e.button.x, e.button.y);
-					List.push_back(new VParticle(List.size(), e.button.x, e.button.y, e.button.x, e.button.y, nx_q, fixed));
+					printf("Add charge -%fq at %d %d\n", nx_q, e.button.x - (SC_WIDTH / 2), e.button.y - (SC_HEIGHT / 2));
+					List.push_back(new VParticle(List.size(), e.button.x - (SC_WIDTH / 2), e.button.y - (SC_HEIGHT / 2), e.button.x - (SC_WIDTH / 2), e.button.y - (SC_HEIGHT / 2), -nx_q, fixed));
 				}
 				else if (e.button.button == SDL_BUTTON_MIDDLE) // Middle-click toggles fixed masses.
 				{
@@ -90,18 +99,29 @@ int _tmain(int argc, _TCHAR* argv[])
 		rk4_integrate();												   // Physics calculations.
 		SDL_SetRenderDrawColor(r, 255, 255, 255, SDL_ALPHA_OPAQUE);        // Clear screen to white color.
 		SDL_RenderClear(r);
+		
+		SDL_SetRenderDrawBlendMode(r, SDL_BLENDMODE_BLEND);
+		SDL_SetRenderDrawColor(r, 0, 0, 0, 50); //Render the lines at lower alpha value, so they are less distracting.
+		for (int x = 0; x < SC_WIDTH; x += 25)
+			SDL_RenderDrawLine(r, x, 0, x, SC_HEIGHT);
+		for (int y = 0; y < SC_HEIGHT; y += 25)
+			SDL_RenderDrawLine(r, 0, y, SC_WIDTH, y);
+		//^^Draw a basic cartesian coordinate grid.
+
 		SDL_Rect pos;
-		VParticle *p = nullptr;
-		for (int i = 0; i < List.size(); i++)
-		{   
-			if (List[i] != nullptr) 
-			{
-				p = List[i];
-				pos = { p->getR()[0] - 1 * SDL_abs(p->getQ()), p->getR()[1] - 1 * SDL_abs(p->getQ()), 2 * SDL_abs(p->getQ()), 2 * SDL_abs(p->getQ()) };
-				(p->getQ() > 0) ? SDL_SetRenderDrawColor(r, 255, 0, 0, SDL_ALPHA_OPAQUE) : SDL_SetRenderDrawColor(r, 0, 0, 255, SDL_ALPHA_OPAQUE);
-				SDL_RenderFillRect(r, &pos);
-			}
-		}									//^^Depending on the charge's magnitude and polarity, render a box centered at the x,y coordinates.
+		for (auto &it : List)
+		{
+			pos.x = (it->getR()[0] + (SC_WIDTH / 2)) - (it->getQ()/2);  //Shift particle coordinates back, so that SDL can draw them
+			pos.y = it->getR()[1] + (SC_HEIGHT / 2) - (it->getQ()/2); //Particles are rendered as squares, with width = |charge|
+			pos.w = SDL_abs(it->getQ());
+			pos.h = SDL_abs(it->getQ());
+			
+			SDL_SetRenderDrawColor(r, it->getQ() > 0 ? 255 : 0, 0, it->getQ() > 0 ? 0 : 255,SDL_ALPHA_OPAQUE);
+			SDL_RenderFillRect(r, &pos);
+			
+			//^^Depending on the charge's magnitude and polarity, render a square centered at the x,y coordinates.
+		}
+		
 		SDL_RenderPresent(r);				// Render to the screen.
 	}
 	SDL_DestroyRenderer(r);					// Clean up allocated SDL resources.
@@ -145,7 +165,7 @@ Vector2D rk4_accl(Vector2D r, Vector2D v, float dt, int id, float q)
 
 void rk4_integrate()
 {
-	for (auto &vp : List) //mad cpp11 features! Loop over every particle in the simulation... (Isn't this an STL feature?)
+	for (auto &vp : List) //mad cpp11 features! Loop over every particle in the simulation...
 	{
 		if (vp->getFixed() == 1) continue; //this charge is fixed in space.
 
@@ -179,6 +199,4 @@ void rk4_integrate()
 		update->setR(update->getR_());
 		update->setV(update->getV_());
 	}
-	// And it is done.
-	// I have achieved nirvana. - arciel.
 }
